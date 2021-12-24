a96b1c06fc02d0075cd9579fabcffcd28d2ffba2
Yuki Ogiwara <yukifluid@gmail.com>
2021-12-24 13:36:20 +0900

    [fix] fix arguments

--- learn.sh
+++ learn.sh
@@ -1,17 +1,22 @@
 # VectorMSE Loss
-python3 main.py mse_loss > ./model/mse_loss/log.txt
+mkdir ./model/vector-mse
+python3 main.py vector-mse > ./model/vector-mse/log.txt
 
 # Composition Loss
-python3 main.py composition_loss_1.0_1.0_1.0 > ./model/composition_loss_1.0_1.0_1.0/log.txt
-python3 main.py composition_loss_0.0_1.0_0.0 > ./model/composition_loss_0.0_1.0_0.0/log.txt
-python3 main.py composition_loss_0.0_0.0_1.0 > ./model/composition_loss_0.0_0.0_1.0/log.txt
-python3 main.py composition_loss_1.0_0.5_0.5 > ./model/composition_loss_1.0_0.5_0.5/log.txt
-python3 main.py composition_loss_1.0_0.5_0.1 > ./model/composition_loss_1.0_0.5_0.1/log.txt
-python3 main.py composition_loss_1.0_0.0_1.0 > ./model/composition_loss_1.0_0.0_1.0/log.txt
-python3 main.py composition_loss_1.0_1.0_0.0 > ./model/composition_loss_1.0_1.0_0.0/log.txt
-python3 main.py composition_loss_1.0_0.0_0.5 > ./model/composition_loss_1.0_0.0_0.5/log.txt
-python3 main.py composition_loss_1.0_0.5_0.0 > ./model/composition_loss_1.0_0.5_0.0/log.txt
-python3 main.py composition_loss_1.0_0.1_0.1 > ./model/composition_loss_1.0_0.1_0.1/log.txt
-python3 main.py composition_loss_1.0_2.0_1.0 > ./model/composition_loss_1.0_2.0_1.0/log.txt
-python3 main.py composition_loss_1.0_1.0_2.0 > ./model/composition_loss_1.0_1.0_2.0/log.txt
-python3 main.py composition_loss_1.0_2.0_2.0 > ./model/composition_loss_1.0_2.0_2.0/log.txt
+mkdier ./model/composition-0.0_1.0_0.0
+python3 main.py composition-0.0_1.0_0.0 0.0 1.0 0.0 > ./model/composition-0.0_1.0_0.0/log.txt
+
+mkdier ./model/composition-0.0_0.0_1.0
+python3 main.py composition-0.0_0.0_1.0 0.0 0.0 1.0 > ./model/composition-0.0_0.0_1.0/log.txt
+
+mkdier ./model/composition-0.5_0.0_0.5
+python3 main.py composition-0.5_0.0_0.5 0.5 0.0 0.5 > ./model/composition-0.5_0.0_0.5/log.txt
+
+mkdier ./model/composition-0.5_0.5_0.0
+python3 main.py composition-0.5_0.5_0.0 0.5 0.5 0.0 > ./model/composition-0.5_0.5_0.0/log.txt
+
+mkdier ./model/composition-0.33_0.33_0.33
+python3 main.py composition-0.33_0.33_0.33 0.33 0.33 0.33 > ./model/composition-0.33_0.33_0.33/log.txt
+
+mkdier ./model/composition-0.5_0.25_0.25
+python3 main.py composition-0.5_0.25_0.25 0.5 0.25 0.25 > ./model/composition-0.5_0.25_0.25/log.txt
